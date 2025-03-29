c91c99217d0ab4e239e3c0f74863a1a823bd7251
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-03-29 15:46:51 +0700

    item detail

--- lib/modules/ecommerce_screen/product_detail_screen.dart
+++ lib/modules/ecommerce_screen/product_detail_screen.dart
@@ -22,10 +22,69 @@ class ProductDetailScreen extends StatelessWidget {
               ))
         ],
       ),
-      body: const Column(
+      body: Column(
         children: [
-          
-          Text("vutriuentirnh")
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 250,
+                    child: PageView.builder(
+                        itemCount: 6,
+                        controller: PageController(viewportFraction: 1),
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Image.asset(
+                            'assets/images/pi1.jpg',
+                            width: double.infinity,
+                            fit: BoxFit.fitHeight,
+                          );
+                        }),
+                  ),
+                  const SizedBox(height: 10,),
+                  const Text("Item name",style: TextStyle(fontSize: 16,color: Colors.grey)),
+                  const SizedBox(height: 10,),
+                  const Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Seller", style: TextStyle(color: Colors.black),),
+                            SizedBox(height: 10,),
+                            Text("In House Brands", style: TextStyle(color: Colors.black, fontSize: 16),),
+
+                          ],
+                        ),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          Icons.message_rounded,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+
+                  ),
+                  const SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      SizedBox(width: 100,child:
+                        Text("Quantity : ",style: TextStyle(),),)
+                    ],
+                  )
+
+                  
+                ],
+              ),
+            ),
+          ))
         ],
       ),
     );

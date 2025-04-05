53b0ab28c5e981a865a715b795d414cd684becc7
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-04-05 21:52:08 +0700

    navigate chat detail

--- lib/component/horizontal_user_list.dart
+++ lib/component/horizontal_user_list.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_social_share/model/user.dart';
 
-class HorizontalUserList extends StatelessWidget {
+import '../screens/messages_screen/chat_detail.dart';
+
+class HorizontalUserList extends StatefulWidget {
   final List<User> users;
 
+
   const HorizontalUserList({super.key, required this.users});
+
+  @override
+  State<HorizontalUserList> createState() => _HorizontalUserListState();
+}
+
+class _HorizontalUserListState extends State<HorizontalUserList> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,31 +25,44 @@ class HorizontalUserList extends StatelessWidget {
       height: 100,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: users.length,
+        itemCount: widget.users.length,
         itemBuilder: (context, index) {
-          final user = users[index];
+          final user = widget.users[index];
 
-          return Container(
-            margin: const EdgeInsets.all(8),
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.blue,
-                  child: Text(
-                    user.username.isNotEmpty ? user.username[0].toUpperCase() : '',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ChatDetail(
+                    userId: user.id,
+                    receiverName: user.username,
                   ),
                 ),
-                const SizedBox(height: 5),
-                Text(
-                  user.username,
-                  style: const TextStyle(fontSize: 12),
-                ),
-              ],
+              );
+            },
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.blue,
+                    child: Text(
+                      user.username.isNotEmpty ? user.username[0].toUpperCase() : '',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    user.username,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
             ),
           );
         },

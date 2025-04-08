37a1892d417eaa46618ed84dc9840423f8d41d3d
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-04-09 01:08:40 +0700

    friend service

---
+++ lib/services/friend_service.dart
@@ -0,0 +1,71 @@
+import 'package:dio/dio.dart';
+import 'api_client.dart';
+
+class FriendService {
+  final Dio _dio = ApiClient.dio;
+
+  Future<Response> addFriend(String requesterId, String receiverId) async {
+    try {
+      final response = await _dio.post(
+        '/users/create-request',
+        data: {
+          'requesterId': requesterId,
+          'receiverId': receiverId,
+        },
+      );
+      return response;
+    } catch (e) {
+      print('Error adding friend: $e');
+      throw Exception('Failed to add friend: $e');
+    }
+  }
+
+  Future<Response> acceptFriend(String friendRequestId) async {
+    try {
+      return await _dio.post('/users/$friendRequestId/accept-friend');
+    } catch (e) {
+      print('Error accepting friend: $e');
+      throw Exception('Failed to accept friend: $e');
+    }
+  }
+
+  Future<Response> deleteFriend(String friendRequestId) async {
+    try {
+      return await _dio.delete('/users/$friendRequestId/delete-friend');
+    } catch (e) {
+      print('Error deleting friend: $e');
+      throw Exception('Failed to delete friend: $e');
+    }
+  }
+
+  Future<Response> getFriends(String userId, {int page = 1, int size = 10}) async {
+    try {
+      return await _dio.get(
+        '/users/$userId/get-friends',
+        queryParameters: {
+          'page': page,
+          'size': size,
+        },
+      );
+    } catch (e) {
+      print('Error getting friends: $e');
+      throw Exception('Failed to get friends: $e');
+    }
+  }
+
+  Future<Response> getFriendRequests(String userId, {int page = 1, int size = 10}) async {
+    try {
+      return await _dio.get(
+        '/users/$userId/get-friends-request',
+        queryParameters: {
+          'page': page,
+          'size': size,
+        },
+      );
+    } catch (e) {
+      print('Error getting friend requests: $e');
+      throw Exception('Failed to get friend requests: $e');
+    }
+  }
+}
+

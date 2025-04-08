288607756d1cf9ab917b110bfddee37eb45807a5
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-04-08 23:59:50 +0700

    m

--- lib/services/topic_service.dart
+++ lib/services/topic_service.dart
@@ -1,3 +1,17 @@
-class TopicService{
+import 'package:dio/dio.dart';
+import 'package:http/http.dart';
 
+import 'api_client.dart';
+
+class TopicService{
+  final Dio _dio = ApiClient.dio;
+
+  Future<Response> createPost(String name, String color) async{
+    final request =
+    try {
+      return await _dio.post('/topic');
+    } catch (e) {
+      throw Exception('Failed to fetch posts: $e');
+    }
+  }
 }

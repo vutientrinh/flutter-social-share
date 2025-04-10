a9e8707766f414e51ce5e9c925552bb2ff891ff3
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-04-10 15:55:09 +0700

    like comment service

--- lib/services/comment_like_service.dart
+++ lib/services/comment_like_service.dart
@@ -16,6 +16,7 @@ class CommentLikeService {
       throw Exception('Failed to like comment: $e');
     }
   }
+
   Future<Response> unlikeAPI(String commentId) async {
     try {
       final response = await _dio.post(
@@ -23,8 +24,20 @@ class CommentLikeService {
       );
       return response;
     } catch (e) {
-      print('Error like comment: $e');
-      throw Exception('Failed to like comment: $e');
+      print('Error unlike comment: $e');
+      throw Exception('Failed to unlike comment: $e');
+    }
+  }
+
+  Future<Response> getLikedUsersAPI(String commentId,
+      {int page = 1, int size = 10}) async {
+    try {
+      final response = await _dio.get('/comments/$commentId/liked-users',
+          data: {"page": page, "size": size});
+      return response;
+    } catch (e) {
+      print('Error get liked users: $e');
+      throw Exception('Failed to get liked users: $e');
     }
   }
 }
